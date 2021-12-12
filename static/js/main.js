ab62c2f154e7980e7956b292b34b217e04eff2c8
adpolischuck <53851696+adpolischuck@users.noreply.github.com>
2021-12-12 02:38:00 +0200

    Add files via upload
    add files

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+webpackJsonp([0],{
+
+/***/ 8:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(9);
+
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/***/ })
+
+},[8]);
+//# sourceMappingURL=main.js.map
